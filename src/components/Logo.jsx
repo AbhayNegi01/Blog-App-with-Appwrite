5659af0af32f0f20135659dd14ccaa8c45fad73a
AbhayNegi01 <abhaynegi21@gmail.com>
2024-10-04 13:04:35 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Logo(width = '100px') {
+  return (
+    <img 
+    src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" 
+    alt="Logo" 
+    className='w-12 border rounded-full transition-transform duration-300 ease-in-out hover:scale-110'
+    />
+  )
+}
+
+export default Logo
