fb5f00c57656f290c2f3726f657852c882089183
AbhayNegi01 <abhaynegi21@gmail.com>
2024-10-06 11:30:44 +0530

    ...

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo(width = '100px') {
   return (
     <img 
-    src="../../public/logo/Logo.jpg" 
+    src="./logo/Logo.jpg" 
     alt="Logo" 
     className='w-12 border rounded-full transition-transform duration-300 ease-in-out hover:scale-110'
     />
